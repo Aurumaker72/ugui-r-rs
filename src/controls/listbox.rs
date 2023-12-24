f703152abaac9f548f1331f976ecfc1f817563bb
Alex <48759429+Aurumaker72@users.noreply.github.com>
2023-12-24 22:53:09 +0100

    Scaffold out listbox implementation

---
+++ src/controls/listbox.rs
@@ -0,0 +1,118 @@
+use crate::controls::visual_state::VisualState;
+use crate::core::messages::Message;
+
+use crate::core::ugui::Ugui;
+use crate::HWND;
+use flagset::FlagSet;
+
+use crate::gfx::styles::{hex_color, Styles};
+use std::collections::HashMap;
+
+#[derive(Clone, Default, Debug)]
+struct ListboxState {
+    /// The current visual state
+    visual_state: VisualState,
+    /// The items
+    items: Vec<String>,
+}
+
+pub fn listbox_style() -> FlagSet<Styles> {
+    Styles::Visible | Styles::Enabled | Styles::Focusable
+}
+pub const LISTBOX_SELECTION_CHANGED: u64 = 50;
+
+/// The message procedure implementation for a listbox
+///
+/// # Arguments
+///
+/// * `ugui`: A reference to the owner Ugui object
+/// * `hwnd`: The source window's handle
+/// * `message`: The message
+///
+/// returns: u64 The message response
+pub fn button_proc(ugui: &mut Ugui, hwnd: HWND, message: Message) -> u64 {
+    let rect = ugui.get_window_rect(hwnd);
+    let mut state: Option<ListboxState> = None;
+    if let Some(data) = ugui.get_udata(hwnd) {
+        state = Some(*(data.downcast::<ListboxState>().unwrap()));
+    }
+
+    match message {
+        Message::StylesChanged => {
+            let style = ugui.get_styles(hwnd);
+            let mut state = ListboxState::default();
+
+            if !style.contains(Styles::Enabled) {
+                state.visual_state = VisualState::Disabled;
+            } else {
+                state.visual_state = VisualState::Normal;
+            }
+
+            ugui.set_udata(hwnd, Some(Box::new(state)));
+        }
+        Message::LmbDown => {
+            state.as_mut().unwrap().visual_state = VisualState::Active;
+            ugui.set_udata(hwnd, Some(Box::new(state.unwrap())));
+            ugui.invalidate_rect(rect);
+            ugui.capture_mouse(hwnd);
+        }
+        Message::LmbUp => {
+            if state.as_mut().unwrap().visual_state == VisualState::Hover {
+                state.as_mut().unwrap().visual_state = VisualState::Normal;
+            } else {
+                state.as_mut().unwrap().visual_state = VisualState::Hover;
+            }
+            ugui.set_udata(hwnd, Some(Box::new(state.unwrap())));
+            ugui.invalidate_rect(rect);
+            ugui.uncapture_mouse(hwnd);
+        }
+        Message::MouseEnter => {
+            if state.as_mut().unwrap().visual_state == VisualState::Hover {
+                state.as_mut().unwrap().visual_state = VisualState::Active;
+            } else {
+                state.as_mut().unwrap().visual_state = VisualState::Hover;
+            }
+            ugui.set_udata(hwnd, Some(Box::new(state.unwrap())));
+            ugui.invalidate_rect(rect);
+        }
+        Message::MouseLeave => {
+            if state.as_mut().unwrap().visual_state == VisualState::Active {
+                state.as_mut().unwrap().visual_state = VisualState::Hover;
+            } else {
+                state.as_mut().unwrap().visual_state = VisualState::Normal;
+            }
+            ugui.set_udata(hwnd, Some(Box::new(state.unwrap())));
+            ugui.invalidate_rect(rect);
+        }
+        Message::Paint => {
+            let rect = ugui.get_window_rect(hwnd);
+
+            let colors = HashMap::from([
+                (
+                    VisualState::Normal,
+                    (hex_color("#E1E1E1"), hex_color("#ADADAD")),
+                ),
+                (
+                    VisualState::Hover,
+                    (hex_color("#E5F1FB"), hex_color("#0078D7")),
+                ),
+                (
+                    VisualState::Active,
+                    (hex_color("#CCE4F7"), hex_color("#005499")),
+                ),
+                (
+                    VisualState::Disabled,
+                    (hex_color("#CCCCCC"), hex_color("#BFBFBF")),
+                ),
+            ]);
+            ugui.paint_quad(
+                rect,
+                colors[&state.as_ref().unwrap().visual_state].0,
+                colors[&state.as_ref().unwrap().visual_state].1,
+                1.0,
+            );
+        }
+        _ => {}
+    }
+    0
+}
--- src/controls/mod.rs
+++ src/controls/mod.rs
@@ -1,5 +1,6 @@
+pub mod button;
+pub mod listbox;
+pub mod scrollbar;
+pub mod textbox;
 pub mod visual_state;
 pub mod window;
-pub mod button;
-pub mod textbox;
-pub mod scrollbar;
