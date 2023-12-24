d7d32f2309d56cf546485760b3e5b64c579a28c5
Alex <48759429+Aurumaker72@users.noreply.github.com>
2023-12-24 20:28:30 +0100

    Add rudimentary scrollbar moving and lock visual state to active while captured

--- src/controls/scrollbar.rs
+++ src/controls/scrollbar.rs
@@ -6,12 +6,9 @@ use crate::core::ugui::Ugui;
 use crate::HWND;
 use flagset::FlagSet;
 
-
 use crate::core::geo::Rect;
 use sdl2::pixels::Color;
 use std::collections::HashMap;
-
-
 
 #[derive(Copy, Clone, Default, Debug)]
 struct ScrollbarState {
@@ -27,7 +24,6 @@ pub fn scrollbar_style() -> FlagSet<Styles> {
     Styles::Visible | Styles::Enabled | Styles::Focusable
 }
 pub fn scrollbar_set(ugui: &mut Ugui, hwnd: HWND, size: f32, value: f32) {
-
     if let Some(data) = ugui.get_udata(hwnd) {
         let state = *(data.downcast::<ScrollbarState>().unwrap());
         ugui.set_udata(
@@ -106,6 +102,17 @@ pub fn scrollbar_proc(ugui: &mut Ugui, hwnd: HWND, message: Message) -> u64 {
             ugui.set_udata(hwnd, Some(Box::new(state.unwrap())));
             ugui.invalidate_rect(rect);
         }
+        Message::MouseMove => {
+            if ugui.get_capture().is_some_and(|x| x == hwnd) {
+                let rect = ugui.get_window_rect(hwnd);
+                let pos = ugui.mouse_state().pos.sub(rect.top_left());
+
+                let frac = pos.y / rect.h;
+                state.as_mut().unwrap().value = frac.clamp(0.0, 1.0);
+                ugui.set_udata(hwnd, Some(Box::new(state.unwrap())));
+                ugui.invalidate_hwnd(hwnd);
+            }
+        }
         Message::Paint => {
             let back_rect = ugui.get_window_rect(hwnd);
             let thumb_rect = Rect {
@@ -134,15 +141,21 @@ pub fn scrollbar_proc(ugui: &mut Ugui, hwnd: HWND, message: Message) -> u64 {
                 ),
             ]);
 
+            let visual_state = if ugui.get_capture().is_some_and(|x| x == hwnd) {
+                VisualState::Active
+            } else {
+                state.unwrap().visual_state
+            };
+
             ugui.paint_quad(
                 back_rect,
-                colors[&state.unwrap().visual_state].0,
+                colors[&visual_state].0,
                 Color::RED,
                 0.0,
             );
             ugui.paint_quad(
                 thumb_rect,
-                colors[&state.unwrap().visual_state].1,
+                colors[&visual_state].1,
                 Color::RED,
                 0.0,
             );
